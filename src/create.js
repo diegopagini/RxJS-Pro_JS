44a321cd5959453dad6f6f4f2e3cf2e36c277cdc
Diego Pagini <diego.pagini@neoris.com>
2022-06-03 11:13:50 +0200

    of

--- src/create.js
+++ src/create.js
@@ -1,9 +1,14 @@
+/** @format */
+import { of } from 'rxjs';
+
 import { displayLog } from './utils';
 
 export default () => {
-    /** start coding */
-
-
-
-    /** end coding */
-}
+	/** start coding */
+	const sourse = of(1, 2, 3, 4, 5, 6);
+	const sourse2 = of([1, 2, 3, 4], 'Hello World!', { foo: 'bar' }, () => {
+		return 'Hi';
+	});
+	const subscription = sourse2.subscribe((data) => displayLog(data));
+	/** end coding */
+};
