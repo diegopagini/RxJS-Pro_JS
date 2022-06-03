a6edf10420b14eee6b9b2572f258d4696089ca06
Diego Pagini <diegopaginidev@gmail.com>
2022-06-03 12:28:04 +0200

    takeLast

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,19 +1,20 @@
-import { displayLog } from './utils';
+/** @format */
 import { fromEvent } from 'rxjs';
-import { map, takeWhile } from 'rxjs/operators';
+import { map, takeLast, takeWhile, tap } from 'rxjs/operators';
+
+import { displayLog } from './utils';
 
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ]),
-        takeWhile( ([col, row]) => col > 3 )
-    );
+	/** start coding */
+	const grid = document.getElementById('grid');
+	const click$ = fromEvent(grid, 'click').pipe(
+		map((val) => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+		takeWhile(([col, row]) => col > 3),
+		tap((val) => console.log(`valid in takeWhile: ${val}`)),
+		takeLast(3)
+	);
 
-    const subscription = click$.subscribe(data => displayLog(data));
+	const subscription = click$.subscribe((data) => displayLog(data));
 
-    /** end coding */
-}
+	/** end coding */
+};
