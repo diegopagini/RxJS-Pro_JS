d9d1f37b5c50442a33a01064bc14c60a73e44d5b
Diego Pagini <diegopaginidev@gmail.com>
2022-06-03 12:34:15 +0200

    reduce

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,20 +1,30 @@
-import { displayLog } from './utils';
+/** @format */
 import { fromEvent } from 'rxjs';
-import { map, takeWhile, tap } from 'rxjs/operators';
+import { map, reduce, takeWhile, tap } from 'rxjs/operators';
+
+import { displayLog } from './utils';
 
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ]),
-        takeWhile( ([col, row]) => col != 0 ),
-        tap(val => console.log(`cell: [${val}]`)),
-    );
+	/** start coding */
+	const grid = document.getElementById('grid');
+	const click$ = fromEvent(grid, 'click').pipe(
+		map((val) => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+		takeWhile(([col, row]) => col != 0),
+		tap((val) => console.log(`cell: [${val}]`)),
+		reduce(
+			(acc, current) => {
+				return {
+					clicks: acc.clicks + 1,
+					cells: [...acc.cells, current],
+				};
+			},
+			{ clicks: 0, cells: [] }
+		)
+	);
 
-    const subscription = click$.subscribe(data => displayLog(data));
+	const subscription = click$.subscribe((data) =>
+		displayLog(`${data.clicks} clicks: ${JSON.stringify(data.cells)}`)
+	);
 
-    /** end coding */
-}
+	/** end coding */
+};
