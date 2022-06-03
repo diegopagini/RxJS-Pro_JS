b8cb9e0412daee1eab8c626af673bb75870eb2b7
Diego Pagini <diego.pagini@neoris.com>
2022-06-03 11:19:13 +0200

    timer && interval

--- src/create.js
+++ src/create.js
@@ -1,9 +1,14 @@
+/** @format */
+import { interval, timer } from 'rxjs';
+
 import { displayLog } from './utils';
 
 export default () => {
-    /** start coding */
+	/** start coding */
+	const source = interval(500);
+	const subscription = source.subscribe((data) => displayLog(data));
 
+	timer(3000).subscribe(() => subscription.unsubscribe());
 
-
-    /** end coding */
-}
+	/** end coding */
+};
