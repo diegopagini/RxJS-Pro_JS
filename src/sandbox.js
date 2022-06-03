9ab4691b755b0f18cb7400d3f93735909a1a8831
Diego Pagini <diegopaginidev@gmail.com>
2022-06-03 14:37:24 +0200

    progress

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,31 +1,38 @@
-import { updateDisplay } from './utils';
+/** @format */
 import { fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, pairwise, tap } from 'rxjs/operators';
+
+import { updateDisplay } from './utils';
 
 export default () => {
-    /** start coding */
-    const progressBar = document.getElementById('progress-bar');
-    const docElement = document.documentElement;
-    const updateProgressBar = (percentage) => {
-        progressBar.style.width = `${percentage}%`;
-    }
+	/** start coding */
+	const progressBar = document.getElementById('progress-bar');
+	const docElement = document.documentElement;
+	const updateProgressBar = (percentage) => {
+		progressBar.style.width = `${percentage}%`;
+	};
 
-    //observable that returns scroll (from top) on scroll events
-    const scroll$ = fromEvent(document, 'scroll').pipe(
-        map(() => docElement.scrollTop),
-        tap(evt => console.log("[scroll]: ", evt))
-    );
+	//observable that returns scroll (from top) on scroll events
+	const scroll$ = fromEvent(document, 'scroll').pipe(
+		map(() => docElement.scrollTop),
+		tap((evt) => console.log('[scroll]: ', evt)),
+		pairwise(),
+		tap(([previous, current]) => {
+			updateDisplay(current > previous ? 'DESC' : 'ASC');
+		}),
+		map(([previous, current]) => current)
+	);
 
-    //observable that returns the amount of page scroll progress
-    const scrollProgress$ = scroll$.pipe(
-        map(evt => {
-            const docHeight = docElement.scrollHeight - docElement.clientHeight;
-            return (evt / docHeight) * 100;
-        })
-    )
+	//observable that returns the amount of page scroll progress
+	const scrollProgress$ = scroll$.pipe(
+		map((evt) => {
+			const docHeight = docElement.scrollHeight - docElement.clientHeight;
+			return (evt / docHeight) * 100;
+		})
+	);
 
-    //subscribe to scroll progress to paint a progress bar
-    const subscription = scrollProgress$.subscribe(updateProgressBar);
+	//subscribe to scroll progress to paint a progress bar
+	const subscription = scrollProgress$.subscribe(updateProgressBar);
 
-    /** end coding */
-}
+	/** end coding */
+};
