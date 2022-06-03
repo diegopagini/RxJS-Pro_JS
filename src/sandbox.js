04841801198ce6eaf2b51042d40b74ed0aca9b44
Diego Pagini <diegopaginidev@gmail.com>
2022-06-03 14:04:13 +0200

    endWidth

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,20 +1,21 @@
-import { displayLog } from './utils';
+/** @format */
 import { fromEvent } from 'rxjs';
-import { map, takeWhile, tap } from 'rxjs/operators';
+import { endWith, map, startWith, takeWhile, tap } from 'rxjs/operators';
+
+import { displayLog } from './utils';
 
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ]),
-        takeWhile( ([col, row]) => col != 0 ),
-        tap(val => console.log(`cell: [${val}]`)),
-    );
+	/** start coding */
+	const grid = document.getElementById('grid');
+	const click$ = fromEvent(grid, 'click').pipe(
+		map((val) => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)]),
+		takeWhile(([col, row]) => col != 0),
+		tap((val) => console.log(`cell: [${val}]`)),
+		startWith('grid dimensions: ', '10x10'), // Lo agrega al comienzo
+		endWith('game finished', 'bye!') // Lo agrega al final.
+	);
 
-    const subscription = click$.subscribe(data => displayLog(data));
+	const subscription = click$.subscribe((data) => displayLog(data));
 
-    /** end coding */
-}
+	/** end coding */
+};
