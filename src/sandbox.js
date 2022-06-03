d1133dc50e4822c0033c63182c0933b219f823e7
Diego Pagini <diegopaginidev@gmail.com>
2022-06-03 11:49:47 +0200

    fromEvent

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,23 @@
+/** @format */
+import { fromEvent } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
 import { displayLog } from './utils';
 
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
+	/** start coding */
+	const grid = document.getElementById('grid');
+	const click$ = fromEvent(grid, 'click');
 
+	const subscription = click$
+		.pipe(
+			map((val) => [
+				Math.floor(val.offsetX / 50),
+				Math.floor(val.offsetY / 50),
+			]),
+			filter((val) => (val[0] + val[1]) % 2 != 0)
+		)
+		.subscribe((data) => displayLog(data));
 
-    /** end coding */
-}
+	/** end coding */
+};
