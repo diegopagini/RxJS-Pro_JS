9e0d795e76fee14aca6f6033326be995835879cf
Diego Pagini <diegopaginidev@gmail.com>
2022-06-03 11:56:23 +0200

    tap

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,18 +1,19 @@
-import { displayLog } from './utils';
+/** @format */
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
+
+import { displayLog } from './utils';
+
 
 export default () => {
-    /** start coding */
-    const grid = document.getElementById('grid');
-    const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ])
-    );
+	/** start coding */
+	const grid = document.getElementById('grid');
+	const click$ = fromEvent(grid, 'click').pipe(
+		tap((evt) => console.log(evt)),
+		map((val) => [Math.floor(val.offsetX / 50), Math.floor(val.offsetY / 50)])
+	);
 
-    const subscription = click$.subscribe(data => displayLog(data));
+	const subscription = click$.subscribe((data) => displayLog(data));
 
-    /** end coding */
-}
+	/** end coding */
+};
