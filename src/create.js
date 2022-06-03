170b3c59250557f4f97a313eb525dbf8a3c4dcc6
Diego Pagini <diegopaginidev@gmail.com>
2022-06-03 11:35:02 +0200

    fromEvent

--- src/create.js
+++ src/create.js
@@ -1,9 +1,20 @@
+/** @format */
+import { fromEvent } from 'rxjs';
+
 import { displayLog } from './utils';
 
 export default () => {
-    /** start coding */
+	/** start coding */
+	const actionBtn = document.getElementById('action-btn');
+	const source = fromEvent(actionBtn, 'click');
 
+	source.subscribe((evt) =>
+		displayLog(`click event at pos (${evt.x}, ${evt.y})`)
+	);
 
+	fromEvent(document, 'mousemove').subscribe((evt) => {
+		console.log(evt);
+	});
 
-    /** end coding */
-}
+	/** end coding */
+};
