0b8ab42d7bcd5d777cab0b99f1c91489691613fb
Diego Pagini <diegopaginidev@gmail.com>
2022-06-04 15:29:57 +0200

    debouncetime

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,19 @@
-import { updateDisplay, displayLog } from './utils';
+/** @format */
+import { fromEvent } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+
+import { displayLog } from './utils';
 
 export default () => {
-    /** start coding */
-    
-    const inputBox = document.getElementById('input-box');
+	/** start coding */
 
-    /** end coding */
-}
+	const inputBox = document.getElementById('input-box');
+	const inputSrc$ = fromEvent(inputBox, 'input').pipe(
+		debounceTime(300), // si dejo de escribir durante 300 ms emitirá.
+		map((event) => event.target.value)
+	);
+
+	inputSrc$.subscribe(displayLog);
+
+	/** end coding */
+};
