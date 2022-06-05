347e166e0a93e0019ae32d1219a2d71cfd15bbbc
Diego Pagini <diegopaginidev@gmail.com>
2022-06-05 16:22:35 +0200

    catch

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ export class api {
 	static getComment(id) {
 		return timer(Math.random() * 1000).pipe(
 			mergeMap((evt) => {
-				const isError = Math.random() > 0.6 ? true : false;
+				const isError = Math.random() > 0.1 ? true : false;
 				if (isError) return throwError(new Error('Request Timeout'));
 				else return of({ id: id, comment: `comment number ${id}` });
 			})
