afb73c487876d7e8235a9f3df3b75bec30b7253e
Diego Pagini <diegopaginidev@gmail.com>
2022-06-05 20:53:06 +0200

    materialize

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,36 +1,40 @@
+/** @format */
+import { EMPTY, fromEvent, interval, merge } from 'rxjs';
+import { dematerialize, mapTo, materialize, scan, startWith, switchMap, takeWhile } from 'rxjs/operators';
+
 import { updateDisplay } from './utils';
-import { fromEvent, interval, merge, EMPTY } from 'rxjs';
-import { mapTo, scan, takeWhile, switchMap, startWith  } from 'rxjs/operators';
 
 export default () => {
-    /** start coding */
+	/** start coding */
 
-    /** number of seconds to init countdown */
-    const countdownSeconds = 10;
-    
-    /** access interface buttons */
-    const pauseButton = document.getElementById('pause-btn');
-    const resumeButton = document.getElementById('resume-btn');
+	/** number of seconds to init countdown */
+	const countdownSeconds = 10;
 
-    /** get comments on button click */
-    const pause$ = fromEvent(pauseButton, 'click');
-    const resume$ = fromEvent(resumeButton, 'click');
-    const isPaused$ = merge(pause$.pipe(mapTo(true)), resume$.pipe(mapTo(false)));
+	/** access interface buttons */
+	const pauseButton = document.getElementById('pause-btn');
+	const resumeButton = document.getElementById('resume-btn');
 
-    /** 1s negative interval */
-    const interval$ = interval(1000).pipe(mapTo(-1));
+	/** get comments on button click */
+	const pause$ = fromEvent(pauseButton, 'click');
+	const resume$ = fromEvent(resumeButton, 'click');
+	const isPaused$ = merge(pause$.pipe(mapTo(true)), resume$.pipe(mapTo(false)));
 
-    /** countdown timer */
-    const countdown$ = isPaused$.pipe(
-        startWith(false),
-        switchMap(paused => !paused ? interval$ : EMPTY),
-        scan((acc, curr) => ( curr ? curr + acc : curr ), countdownSeconds),
-        takeWhile(v => v >= 0)
-    );
+	/** 1s negative interval */
+	const interval$ = interval(1000).pipe(mapTo(-1));
 
-    /** subscribe to countdown */
-    countdown$.subscribe(updateDisplay);
+	/** countdown timer */
+	const countdown$ = isPaused$.pipe(
+		startWith(false),
+		switchMap((paused) =>
+			!paused ? interval$.pipe(materialize()) : EMPTY.pipe(materialize())
+		),
+		dematerialize(),
+		scan((acc, curr) => (curr ? curr + acc : curr), countdownSeconds),
+		takeWhile((v) => v >= 0)
+	);
 
-    
-    /** end coding */
-}
+	/** subscribe to countdown */
+	countdown$.subscribe(updateDisplay);
+
+	/** end coding */
+};
