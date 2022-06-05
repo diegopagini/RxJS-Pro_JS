b8830a401505060c0ae5601b78c2fd74ce3ca8f7
Diego Pagini <diegopaginidev@gmail.com>
2022-06-05 15:36:27 +0200

    concat

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
+/** @format */
 import { timer } from 'rxjs';
 import { mapTo } from 'rxjs/operators';
 
-export class api{
-    static getComment(id){
-        return timer(Math.random()*1000).pipe(
-            mapTo({id:id, comment:`comment number ${id}`})
-        );
-    }
+export class api {
+	static getComment(id) {
+		return timer(Math.random() * 1000).pipe(
+			mapTo({ id: id, comment: `comment number ${id}` })
+		);
+	}
 }
--- src/sandbox.js
+++ src/sandbox.js
@@ -1,32 +1,38 @@
-import { updateDisplay, displayLog } from './utils';
+/** @format */
+import { concat, fromEvent } from 'rxjs';
+import { endWith, map } from 'rxjs/operators';
+
 import { api } from './api';
-import { merge, fromEvent } from 'rxjs';
-import { map, endWith } from 'rxjs/operators';
+import { displayLog } from './utils';
 
 export default () => {
-    /** start coding */
-    
-    const button = document.getElementById('btn');
+	/** start coding */
 
-    /** get 4 consecutive comments */
-    const getComments = () =>{
-        //get observables from fake REST API.
-        const comment1$ = api.getComment(1);
-        const comment2$ = api.getComment(2);
-        const comment3$ = api.getComment(3);
-        const comment4$ = api.getComment(4);
+	const button = document.getElementById('btn');
 
-        //subscribe to all the observables to get and display comments
-        merge(comment1$, comment2$, comment3$, comment4$).pipe(
-            map(({id, comment}) => `#${id} - ${comment}`),
-            endWith('--------//--------')
-        ).subscribe(data =>{
-            displayLog(data);
-        })
-    }
+	/** get 4 consecutive comments */
+	const getComments = () => {
+		//get observables from fake REST API.
+		const comment1$ = api.getComment(1);
+		const comment2$ = api.getComment(2);
+		const comment3$ = api.getComment(3);
+		const comment4$ = api.getComment(4);
 
-    /** get comments on button click */
-    fromEvent(button, 'click').subscribe(getComments);
+		//subscribe to all the observables to get and display comments
+		concat(comment1$, comment2$, comment3$, comment4$) // contat para traer todos los observables en orden
+			// solo cuando se complete el primer obsevable llamara al siguiente
+			// por otro lado forkJoin esperara a que todos se completen y los devolverá en un array.
+			.pipe(
+				map(({ id, comment }) => `#${id} - ${comment}`),
+				endWith('--------//--------')
+			)
+			.subscribe((data) => {
+				displayLog(data);
+			});
+	};
 
-    /** end coding */
-}
+	/** get comments on button click */
+	fromEvent(button, 'click').subscribe(getComments);
+
+	/** end coding */
+};
